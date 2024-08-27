99084a6bb8bf4e6b02eb7804252674dfa9a96134
saurabhack <kamanesaurabh68@gmail.com>
2024-08-27 12:32:36 +0530

    connect with mongodb

--- firstProject/index.js
+++ firstProject/index.js
@@ -1,47 +1,100 @@
 const express = require('express')
 const users=require('./MOCK_DATA.json')
 const fs=require("fs")
-
+const mongoose = require("mongoose")
 const app=express()
 const port=3000
 
-app.use(express.urlencoded({extended:false}))
+//connection
 
-app.get('/api/users',(req,res)=>{
-    return res.json(users)
+mongoose
+.connect('mongodb://127.0.0.1:27017/UsersData')
+.then(()=>{
+    console.log("Mongodb connected")
+})
+.catch((err)=>{
+    console.log("MongoDb Error ", err)
 })
 
-app.get('/users',(req,res)=>{
+
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+    },
+    lastName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    jobTitle:{
+        type:String,
+        require:true
+    },
+    gender:{
+        type:String,
+    }
+
+},{timestamps:true})
+
+const User=mongoose.model('user',userSchema)
+
+app.use(express.urlencoded({extended:false}))
+app.use((req,res,next)=>{
+    fs.appendFile('log.txt',`\n${Date.now()}: ${req.method} : ${req.path}\n`,(err,data)=>{
+        next()
+    })
+    
+});
+
+app.get('/api/users', async (req,res)=>{
+    const allDBusers=await User.find({})
+    return res.json(allDBusers)
+})
+
+app.get('/users',   async (req,res)=>{
+    const allDBUsers = await User.find({});
     const html=`
     <ul>
-    ${users.map((user)=> `<li>${user.first_name}</li>`).join("")}
+    ${allDBUsers.map((user)=> `<li>${user.firstName} - ${user.email}</li>`).join("")}
     </ul>
     `
     res.send(html)
 })
 
-app.route("/api/users/:id").get((req,res)=>{
-    const id=Number(req.params.id)
-    const user=users.find((user)=>user.id===id)
-    return res.json(user)
-})
-.patch((res,req)=>{
-    //user edit with id
-    return res.json({status:"pending"})
-})
-.delete((req,res)=>{
+app.route("/api/users/:id").get(async(req,res)=>{
+    const user = await User.findById(req.params.id)
+    if(!user) return res.status(404).json({error:"user not found"});
+    return res.json(user);
+}).patch( async (req,res)=>{
+    await User.findByIdAndUpdate(req.params.id,{lastName:"Changed"})
+    return res.json({status:"Success"})
+}).delete(async(req,res)=>{
         //delete edit with id
-        return res.json({status:"pending"})
+        await User.findByIdAndDelete(req.params.id)
+        return res.json({status:"success"})
 })
 
 
-app.post("/api/users",(req,res)=>{
+app.post("/api/users",  async (req,res)=>{
     const body=req.body;
-    users.push({...body, id: users.length+1})
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-        res.json({status:"success",id:users.length})
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({msg:"All fields are req ..."});
+    }
+
+    const result = await User.create({
+        firstName:body.first_name,
+        lastName:body.last_name,
+        email:body.email,
+        gender:body.gender,
+        jobTitle:body.job_title
     })
-    console.log(body)
+    console.log(result)
+    return res.status(201).json({msg:"success"});
 
 })
 
